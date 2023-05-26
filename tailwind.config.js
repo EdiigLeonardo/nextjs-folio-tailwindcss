163c3bdc0edacba41d0a65f0fb7c45b1c6d6f184
EdiigLeonardo <65238422+EdiigLeonardo@users.noreply.github.com>
2023-05-26 14:54:55 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary-color': '#4571BD',
+        'secondary-color': '#1A073A',
+        'tertiary-color': '#ecf0f3',
+      }
+    },
+  },
+  plugins: [],
+};
