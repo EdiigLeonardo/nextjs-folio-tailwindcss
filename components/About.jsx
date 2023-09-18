89f845e2f8ee9cbe5ff54b2da43eb0bdfe21bef0
EdiigLeonardo <65238422+EdiigLeonardo@users.noreply.github.com>
2023-09-18 15:51:12 +0100

    config new classes

--- components/About.jsx
+++ components/About.jsx
@@ -12,23 +12,26 @@ const About = () => {
             About
           </p>
           <h2 className='py-4'>Who I Am</h2>
-          <p className='py-2 text-gray-600 text-justify'>
-          With a strong foundation in JavaScript, SQL, and Python, I bring a versatile skill set to the table. 
-          I have hands-on experience with a range of frameworks and libraries, including React, Angular, Node.js, Express.js, Flask, and Next.js, enabling me to tackle diverse development projects with confidence. 
-          My proficiency extends to essential tools such as Git, Rest, Jira, and Redux, facilitating efficient collaboration and streamlined workflow. 
-          Additionally, I possess expertise in front-end technologies like HTML/CSS, Tailwind CSS, and SCSS, enabling me to create visually appealing and responsive user interfaces. 
-          Furthermore, I have knowledge of containerization technologies such as Docker and orchestration platforms like Kubernetes, allowing me to optimize deployment and scalability. 
-          With this comprehensive skill set, I am well-equipped to contribute to dynamic and challenging software development initiatives.
-          </p>
+          <div className='flex text-justify justify-around items-center gap-6 max-h-full'>
+            <p className='py-2 text-gray-600 text-justify'>
+              With a strong foundation in JavaScript, SQL, and Python, I bring a versatile skill set to the table.
+              I have hands-on experience with a range of frameworks and libraries, including React, Angular, Node.js, Express.js, Flask, and Next.js, enabling me to tackle diverse development projects with confidence.
+              My proficiency extends to essential tools such as Git, Rest, Jira, and Redux, facilitating efficient collaboration and streamlined workflow.
+              Additionally, I possess expertise in front-end technologies like HTML/CSS, Tailwind CSS, and SCSS, enabling me to create visually appealing and responsive user interfaces.
+              Furthermore, I have knowledge of containerization technologies such as Docker and orchestration platforms like Kubernetes, allowing me to optimize deployment and scalability.
+              With this comprehensive skill set, I am well-equipped to contribute to dynamic and challenging software development initiatives.
+            </p>
+            <div className='w-full h-full m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300 md-max:hidden'>
+              <Image src={AboutImg} className='w-full h-full rounded-xl' alt='/' />
+            </div>
+          </div>
           <Link href='/#projects'>
             <p className='py-2 text-gray-600 underline cursor-pointer'>
               Check out some of my latest projects.
             </p>
           </Link>
         </div>
-        {/* <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-          <Image src={AboutImg} className='rounded-xl h-auto' alt='/' /> 
-        </div> */}
+
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,16 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'md-max': { 'max': '850px' }, // Define the custom breakpoint here
+      },
       colors: {
         'primary-color': '#4571BD',
         'secondary-color': '#1A073A',
         'tertiary-color': '#ecf0f3',
-      }
+      },
     },
   },
   plugins: [],
 };
+
